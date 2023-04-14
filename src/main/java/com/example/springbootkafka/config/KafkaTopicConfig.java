49d75444e02b33e96e3f6cb9e7c0aab515190b54
yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-14 20:20:57 +0600

    first commit

---
+++ src/main/java/com/example/springbootkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.example.springbootkafka.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic comexampleTopic(){
+        return TopicBuilder.name("comexample").build();
+    }
+}
