6cb4fa10f5ff4ff806942c0e2d557f76b9fab569
yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-04-15 08:32:44 +0600

    created kafka producer to produce JSON Message

---
+++ src/main/java/com/example/springbootkafka/kafka/JsonKafkaProducer.java
@@ -0,0 +1,34 @@
+package com.example.springbootkafka.kafka;
+
+
+import com.example.springbootkafka.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void Message(User data){
+        LOGGER.info(String.format("Message sent -> %s", data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "comexample")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
